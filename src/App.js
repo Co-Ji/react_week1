608343ec8394a8df271bf965d0f54cbbfb753b4b
Co-Ji <100311029+Co-Ji@users.noreply.github.com>
2022-03-21 16:11:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container>
+            <h2 style={{ textAlign: "center" }}>내 일주일은?</h2>
+            <WeekBox>
+                <Week>월</Week>
+                <Score></Score>
+                <Score></Score>
+                <Score></Score>
+                <Score></Score>
+                <Score></Score>
+                <Triangle></Triangle>
+            </WeekBox>
+        </Container>
+    );
 }
 
+const Container = styled.div`
+    background: aliceblue;
+    width: 60vw;
+    maxwidth: 400px;
+    height: 90vh;
+    margin: 40px auto;
+    padding: 20px;
+    box-sizing: border-box;
+`;
+
+const WeekBox = styled.div`
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+const Week = styled.div`
+    width: 25px;
+    height: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Score = styled.div`
+    width: 25px;
+    height: 25px;
+    background-color: lightgray;
+    border-radius: 50px;
+`;
+
+const Triangle = styled.div`
+width: 0px;
+height: 0px;
+border-bottom: 20px solid #007fff;
+border-left: 12.5px solid transparent;
+border-right: 12.5px solid transparent;
+transform: rotate( 90deg);
+
+}
+`;
 export default App;
